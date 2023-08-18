83f02eb5c0001b517a9022ad9e5dc9b1c75a27d5
Stefanos Stefanidis <46319359+calmsacibis995@users.noreply.github.com>
2023-08-18 18:14:14 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.temperature() > 30) {
+        basic.showString("hot!")
+    }
+    if (input.temperature() > 40) {
+        basic.showString("Very hot!")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+})
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
